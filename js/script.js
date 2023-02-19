19ba741c62b807aa91f3d8621d3a4a5070ce0953
Lucas Dionisi <lucas.dionisi@gmail.com>
2023-02-19 20:13:01 +0100

    New icons

--- js/script.js
+++ js/script.js
@@ -2,7 +2,37 @@ const musicsList = [
     {
         elementId: 'MoneyDzy',
         url: '../resources/songs/audio.mp3',
+        type: Type.Follow,
+        title: 'Money Dzydzy'
+    },
+    {
+        elementId: 'MoneyDzy2',
+        url: '../resources/songs/audio.mp3',
+        type: Type.Subscriber,
+        title: 'Money Dzydzy'
+    },
+    {
+        elementId: 'MoneyDzy3',
+        url: '../resources/songs/audio.mp3',
         type: Type.Donation,
+        title: 'Money Dzydzy'
+    },
+    {
+        elementId: 'MoneyDzy4',
+        url: '../resources/songs/audio.mp3',
+        type: Type.Bit,
+        title: 'Money Dzydzy'
+    },
+    {
+        elementId: 'MoneyDzy5',
+        url: '../resources/songs/audio.mp3',
+        type: Type.Raid,
+        title: 'Money Dzydzy'
+    },
+    {
+        elementId: 'MoneyDzy6',
+        url: '../resources/songs/audio.mp3',
+        type: Type.Ban,
         title: 'Money Dzydzy'
     }
 ];
